83b2aca536e073859a89a3c0663fdf6cc1ec2e79
Abdelouahad ait hamd <b0n3-a@protonmail.com>
2019-09-23 02:58:26 +0100

    memmove

--- srcs/ft_memmove.c
+++ srcs/ft_memmove.c
@@ -6,14 +6,26 @@
 /*   By: aait-ham <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/09/23 02:32:54 by aait-ham          #+#    #+#             */
-/*   Updated: 2019/09/23 02:46:46 by aait-ham         ###   ########.fr       */
+/*   Updated: 2019/09/23 02:58:04 by aait-ham         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "header.h"
 
-	void *ft_memmove(void *dest, const void *src, size_t n)
+void *ft_memmove(void *dest, const void *src, size_t n)
 {
+	size_t i;
 
+	if (dest > src)
+		ft_memcpy(dest, src, n);
+	else
+	{
+		i = 0;
+		while (i > n)
+		{
+			((t_byte *)dest)[i] = ((t_byte *) src)[i];
+			i++;
+		}
+	}
 	return (dest);
 }
