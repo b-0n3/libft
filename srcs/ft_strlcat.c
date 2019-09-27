98136f503e211f99aec74617eb559ad97ae5bdd0
Abdelouahad ait hamd <b0n3-a@protonmail.com>
2019-09-27 02:19:29 +0100

    all

--- srcs/ft_strlcat.c
+++ srcs/ft_strlcat.c
@@ -1,32 +1,30 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_strstr.c                                        :+:      :+:    :+:   */
+/*   ft_strlcat.c                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: aait-ham <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2019/09/24 23:02:52 by aait-ham          #+#    #+#             */
-/*   Updated: 2019/09/26 02:54:29 by aait-ham         ###   ########.fr       */
+/*   Created: 2019/09/26 23:17:40 by aait-ham          #+#    #+#             */
+/*   Updated: 2019/09/27 01:44:25 by aait-ham         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-char	*ft_strstr(const char *haystack, const char *needle)
+size_t	ft_strlcat(char *dst, const char *src, size_t size)
 {
+
 	size_t	i;
-	
-	if (ft_strlen(needle) == 0)
-		return ((char *) haystack);
-	while (haystack)
-	{
-		i = 0;
-		while (haystack[i] == needle[i])
-		{
-			if (needle[++i] == '\0')
-				return ((char *) haystack);
-		}
-		haystack++;
-	}
-	return(NULL);
+	size_t	src_size;
+	size_t	dest_size;
+
+	i = 0;
+	src_size = ft_strlen(src);
+	dest_size = ft_strlen(dst); 
+	i = dest_size;
+	while (*src && i + 1 < size)
+		dst[i++] = *(src++);
+	dst[i] = 0;
+	return (src_size + ((dest_size > size) ? size : dest_size));
 }
--- srcs/ft_strnstr.c
+++ srcs/ft_strnstr.c
@@ -1,32 +1,13 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_strstr.c                                        :+:      :+:    :+:   */
+/*   ft_strnstr.c                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: aait-ham <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2019/09/24 23:02:52 by aait-ham          #+#    #+#             */
-/*   Updated: 2019/09/26 02:54:29 by aait-ham         ###   ########.fr       */
+/*   Created: 2019/09/27 01:58:55 by aait-ham          #+#    #+#             */
+/*   Updated: 2019/09/27 01:58:57 by aait-ham         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "libft.h"
 
-char	*ft_strstr(const char *haystack, const char *needle)
-{
-	size_t	i;
-	
-	if (ft_strlen(needle) == 0)
-		return ((char *) haystack);
-	while (haystack)
-	{
-		i = 0;
-		while (haystack[i] == needle[i])
-		{
-			if (needle[++i] == '\0')
-				return ((char *) haystack);
-		}
-		haystack++;
-	}
-	return(NULL);
-}
