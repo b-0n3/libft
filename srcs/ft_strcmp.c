e3ad67c90efb27b3648631ddefed4a16867ac29e
Abdelouahad ait hamd <b0n3-a@protonmail.com>
2019-09-28 17:17:02 +0100

    all

--- srcs/ft_strcmp.c
+++ srcs/ft_strcmp.c
@@ -1,31 +1,29 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_strnstr.c                                       :+:      :+:    :+:   */
+/*   ft_strcmp.c                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: aait-ham <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2019/09/27 01:58:55 by aait-ham          #+#    #+#             */
-/*   Updated: 2019/09/27 16:09:30 by aait-ham         ###   ########.fr       */
+/*   Created: 2019/09/28 15:51:56 by aait-ham          #+#    #+#             */
+/*   Updated: 2019/09/28 17:13:59 by aait-ham         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-char	*strnstr(const char *haystack, const char *needle, size_t n)
+int	ft_strcmp(const char *s1, const char *s2)
 {
-	size_t	i;
-	size_t	index;
+	size_t	s1_size;
+	size_t	s2_size;
+	int	i;
 
-	i = 0;
-	while (haystack)
-	{
-		index = 0;
-		while ((haystack[index] == needle[index]) && (i + index < n))
-			if (!needle[index])
-				return ((char *) haystack);
-		haystack++;
-		i++;
-	}
+	s1_size = ft_strlen(s1);
+	s2_size = ft_strlen(s2);
+	i = ft_memcmp(s1, s2, (s1_size > s2_size) ? s2_size : s1_size);
+	if (i != 0)
+		return ( (i > 0) ? 1 : -1);
+	if (s1_size != s2_size)
+		return (( s1_size > s2_size) ? 1 : -1);
 	return (0);
 }
